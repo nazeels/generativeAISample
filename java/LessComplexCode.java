46bc528b536a583523c7a9c28b23a6437ecfb2eb
Nazeel <nazeel.ak@gmail.com>
2023-07-05 15:01:09 +0530

    Create LessComplexCode.java

---
+++ java/LessComplexCode.java
@@ -0,0 +1,43 @@
+public class SimpleCode {
+    public static void main(String[] args) {
+        int number = 10;
+        int result = 0;
+
+        if (number > 0 && number < 5) {
+            switch (number) {
+                case 1:
+                    result = 10;
+                    break;
+                case 2:
+                    result = 20;
+                    break;
+                case 3:
+                    result = 30;
+                    break;
+                case 4:
+                    result = 40;
+                    break;
+            }
+        } else if (number == 5) {
+            for (int i = 0; i < 5; i++) {
+                result += i;
+            }
+        } else if (number > 0) {
+            while (number > 0) {
+                if (number % 2 == 0) {
+                    result -= number;
+                } else {
+                    result += number;
+                }
+                number--;
+            }
+        } else {
+            do {
+                result -= number;
+                number++;
+            } while (number < 0);
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
